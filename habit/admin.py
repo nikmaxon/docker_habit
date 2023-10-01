46a459b7059dac4b89dc6d01ca27eed7f07c3aa2
nikmaxon <nikmakson99@gmail.com>
2023-10-01 22:35:33 +0300

    Доработки

---
+++ habit/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from habit.models import PleasantHabit, UnpleasantHabit
+
+admin.site.register(PleasantHabit)
+
+admin.site.register(UnpleasantHabit)
---
+++ habit/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class HabitConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'habit'
+    verbose_name = 'Привычки'
